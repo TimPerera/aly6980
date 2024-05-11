cdf05cda1b429356dd5d32862cda719ed5a2299d
Timothy Perera <timothy@timothys-mbp.home>
2024-05-10 20:04:11 -0400

    Add logic to update assessment type to closing if last assessment was more than three months ago.

--- data_exploration.py
+++ data_exploration.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import openpyxl
 import logging
+from datetime import datetime, timedelta
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -11,13 +12,8 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 c_handler.setFormatter(formatter)
 logger.addHandler(c_handler)
 
-times_data = pd.read_excel('TIMES.xlsx')
-demographics_data = pd.read_excel('ParticipantDemographics.xlsx')
-initiations_data = pd.read_excel('ProgramInitiations.xlsx')
-terminations_data = pd.read_excel('ProgramTerminations.xlsx')
-servicedel_data = pd.read_excel('ServiceDeliveries.xlsx')
 
-logger.debug(f'TIMES Data Rows: {len(times_data)}')
+
 
 # Raw TIMES total Score/ (# of scored indicators * 5)
 def calculate_scaled_times(rows):
@@ -38,6 +34,8 @@ def fix_assessment_type(group):
     Defines rules for how to deal with missing or irregular Assessment Types
     1) First entry is Baseline
     2) Convert any later observations identified as a Baseline to Quarterly
+    3) Any subsequent observations not labelled will be Quarterly
+
     """
     a_types = list(group['Assessment Type'])
     if pd.isna(a_types[0]): # if first entry is Null then make it Baseline
@@ -45,27 +43,46 @@ def fix_assessment_type(group):
     if 'Baseline' in a_types[1:]: # Baseline entry exists after first entry. Replace that with 'Quarterly'
         idx = a_types[1:].index('Baseline')
         a_types[idx+1] = 'Quarterly'
+    a_types = ['Quarterly' if pd.isna(a_type) else a_type for a_type in a_types] # if any further missing labels exist replace it with Quarterly
+
+    current_date = datetime.now()
+    if len(a_types)>1 and a_types[-1] != 'Closing':
+        list(group['Assessment Date'])[-1] - current_date > timedelta(weeks=13.04) # Last assessment was more than 3 months ago. Change last entry to Closing
+        a_types[-1] = 'Closing'
+
     
     group['Assessment Type'] = a_types
     return group
 
-times_data = times_data.rename(columns={'Participant: Participant ID  ↑':'Participant ID'})
-# Calculate Scaled TIMES Score Column
-scaled_times = times_data.apply(calculate_scaled_times, axis=1).reset_index(drop=True)
-times_data.insert(3, 'Scaled TIMES Score',scaled_times)
+def main():
+    # Calculate Scaled TIMES Score Column
+    times_data = pd.read_excel('TIMES.xlsx')
+    demographics_data = pd.read_excel('ParticipantDemographics.xlsx')
+    initiations_data = pd.read_excel('ProgramInitiations.xlsx')
+    terminations_data = pd.read_excel('ProgramTerminations.xlsx')
+    servicedel_data = pd.read_excel('ServiceDeliveries.xlsx')
+    scaled_times = times_data.apply(calculate_scaled_times, axis=1).reset_index(drop=True)
+    times_data.insert(3, 'Scaled TIMES Score',scaled_times)
+    times_data = times_data.rename(columns={'Participant: Participant ID  ↑':'Participant ID',
+                                            'Assessment Completed Date  ↑':'Assessment Date'})
+    
+    times_data['Assessment Date'] = pd.to_datetime(times_data['Assessment Date'],format='%Y-%m-%d')
+    logger.debug(f'TIMES Data Rows: {len(times_data)}')
+    # Fill in missing participant id 
+    times_data['Participant ID'] = times_data['Participant ID'].fillna(method='ffill')
 
-# Fill in missing participant id 
-times_data['Participant ID'] = times_data['Participant ID'].fillna(method='ffill')
+    # Deal with missing Assessment Types
+    times_df = times_data.groupby('Participant ID').apply(fix_assessment_type).reset_index(drop=True)
+    times_df.to_csv('transformed_times.csv')
+    # Left Join on All other data sets
+    #times_demo = pd.merge(times_df, demographics_data, on='Participant ID',how='left')
+    # times_demo_init = pd.merge(times_df, initiations_data, on = 'Participant ID', how='left')
+    # times_demo_init_term = pd.merge(times_demo_init, terminations_data, on='Participant ID',how='left')
+    # times_demo_init_term_serv = pd.merge(times_demo_init_term, servicedel_data, on='Participant ID',how='left')
 
-# Deal with missing Assessment Types
-times_df = times_data.groupby('Participant ID').apply(fix_assessment_type).reset_index(drop=True)
+    #logger.debug(f'Final Df length:{len(times_demo_init_term_serv)}')
 
-# Left Join on All other data sets
-times_demo = pd.merge(times_df, demographics_data, on='Participant ID',how='left')
-times_demo_init = pd.merge(times_demo, initiations_data, on = 'Participant ID', how='left')
-times_demo_init_term = pd.merge(times_demo_init, terminations_data, on='Participant ID',how='left')
-times_demo_init_term_serv = pd.merge(times_demo_init_term, servicedel_data, on='Participant ID',how='left')
+    #times_demo_init_term_serv.to_csv('transformed_data.csv')
 
-logger.debug(f'Final Df length:{len(times_demo_init_term_serv)}')
-
-times_demo_init_term_serv.to_csv('transformed_data.csv')
+if __name__=='__main__':
+    main()
